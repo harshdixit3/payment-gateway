daa62f18eb12d0155cacebb3e1cb878542af991f
harshdixit3 <harsh.dixit@3gmail.com>
2024-10-22 20:02:56 +0530

    Designing Payment-gateway UI using React-typescript

---
+++ payment-gateway/src/Components/App.tsx
@@ -0,0 +1,12 @@
+import Menu from "./Menu";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Choose your mode of payment and Complete the purchases</h1>
+      <Menu />
+    </div>
+  );
+};
+
+export default App;
