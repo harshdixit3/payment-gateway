b618d2be36cd5367a5feaac6d277a0a645d85a96
harshdixit3 <harsh.dixit@3gmail.com>
2024-11-05 21:25:42 +0530

    adding card Form

--- payment-gateway/src/Components/Card.tsx
+++ payment-gateway/src/Components/Card.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useState } from "react";
 
 const Card = () => {
+  const [cardNumber, seCardNumber] = useState("");
+
   return (
-    <div>Card</div>
-  )
-}
+    <div>
+      <form>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <label htmlFor="cardNumber">
+                  Credit Card Number: (12 Digit number)
+                </label>
+              </td>
+              <td>
+                <input
+                  type="number"
+                  id="cardNumber"
+                  min="1000000000000"
+                  max="999999999999"
+                  name="cardNumber"
+                  value={cardNumber}
+                  onChange={( event ) => seCardNumber( event.target.value)}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+    </div>
+  );
+};
 
 export default Card;
