ee45a8330b5e0f1c468679d10bd051b5df1e60a1
harshdixit3 <harsh.dixit@3gmail.com>
2024-11-09 13:14:57 +0530

    Completed the UPI Component Payment Gateway is Completed

--- payment-gateway/src/Components/UPI.tsx
+++ payment-gateway/src/Components/UPI.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { FormEvent, useState } from "react";
+import ConfermationDialog from "./ConfermationDialog";
+import PaymentsOptions from "../Enums/PaymentsOptions";
 
 const UPI = () => {
+  const [upi, setUpi] = useState("");
+
+  const [showConfermationDialog, setshowConfermationDialog] = useState(false);
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setshowConfermationDialog(true);
+  };
+
+  const closeDialog = () => {
+    setshowConfermationDialog(false);
+  };
+
   return (
-    <div>UPI</div>
-  )
-}
+    <div>
+      <form onSubmit={onSubmit}>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <label htmlFor="cardNumber">UPI ID:</label>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  id="upi"
+                  name="upi"
+                  value={upi}
+                  onChange={(event) => setUpi(event.target.value)}
+                />
+              </td>
+            </tr>
+            <td>
+              <button className="btn btn-pay">Pay</button>
+            </td>
+          </tbody>
+        </table>
+      </form>
+      {showConfermationDialog && (
+        <ConfermationDialog
+          onClickNo={closeDialog}
+          paymentOption={PaymentsOptions.CARD}
+        />
+      )}
+    </div>
+  );
+};
 
 export default UPI;
